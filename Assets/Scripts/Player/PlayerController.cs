7d7510ac786ea0e51e25a7f6cc70ea6c0e2f00a6
Kosei Hirano <mathko314@gmail.com>
2018-09-15 15:20:35 +0900

    walk & run

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FPS{
+	public enum PlayerState{
+		Idle, Walking, Running, Jumping
+	}
+
+	[RequireComponent(typeof(CharacterController), typeof(AudioSource))]
+	public class PlayerController: MonoBehaviour{
+		[Range(0.1f, 2f)]
+		public float walkSpeed = 1.5f;
+		[Range(0.1f, 10f)]
+		public float runSpeed = 3.5f;
+
+		// Character Controller
+		private CharacterController charaController;
+
+		void Start(){
+			charaController = GetComponent<CharacterController>();
+		}
+
+		void Update(){
+			Move();
+		}
+
+		void Move(){
+			float moveH = Input.GetAxis("Horizontal");
+			float moveV = Input.GetAxis("Vertical");
+			Vector3 movement = new Vector3(moveH, 0, moveV);
+
+			if(movement.sqrMagnitude > 1){
+				movement.Normalize();
+			}
+			
+			// Run
+			if(Input.GetKey(KeyCode.LeftShift)||Input.GetKey(KeyCode.RightShift)){
+				charaController.Move(movement * Time.fixedDeltaTime * runSpeed);
+			}
+			else{
+				charaController.Move(movement * Time.fixedDeltaTime * walkSpeed);
+			}
+		}
+	}
+}
