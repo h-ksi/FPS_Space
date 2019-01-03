ee8b108ebb275b204dc8cdd11a10b30af49b16e9
Kosei Hirano <mathko314@gmail.com>
2019-01-03 21:57:31 +0900

    add scripts for shooting bullets

---
+++ Assets/Scripts/Player/BulletPool.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPool : MonoBehaviour
+{
+    const float SHOT_SPEED = 100f;
+
+    [SerializeField] GameObject _pollObject;    
+    int _polledAmount = 20;
+    [SerializeField] List<GameObject> _pollList;
+    int _readyNum = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        _pollList = new List<GameObject>();
+
+        for (int i = 0; i < _polledAmount; i++)
+        {
+            GameObject　_bullet = Instantiate(_pollObject, transform.position, Quaternion.identity);            
+            _bullet.SetActive(false);
+            _bullet.transform.parent = this.gameObject.transform;
+            _bullet.transform.Rotate(transform.right, 90);
+            _pollList.Add(_bullet);
+        }
+    }
+
+    public void ShootBullet()
+    {                
+        if(_readyNum == _polledAmount)
+        {
+            _readyNum = 0;
+        }
+
+        // その弾丸の初回使用時にアクティブにする
+        if(!_pollList[_readyNum].activeSelf)
+        {
+            _pollList[_readyNum].SetActive(true);
+        }
+        // 再利用弾を銃口にセット
+        else
+        {
+            _pollList[_readyNum].transform.position = transform.position;
+            _pollList[_readyNum].transform.rotation = transform.rotation;
+            _pollList[_readyNum].transform.Rotate(transform.right, 90);
+        }
+        
+        // 発射
+        _pollList[_readyNum].GetComponent<Rigidbody>().velocity = transform.forward * SHOT_SPEED;
+        _readyNum++;         
+    }
+}
---
+++ Assets/Scripts/Player/GunShooter.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunShooter : MonoBehaviour 
+{
+	const float SHOT_INTERVAL = 0.1f;
+	const float MAX_RAY_DISTANCE = 100f;
+
+	[SerializeField] BulletPool _bulletPool;
+	[SerializeField] GameObject _hitTarget;
+	[SerializeField] GameObject _ricochetFirePrefab;
+    [SerializeField] ParticleSystem _gunFireParticle;
+	
+	Ray _ray;
+	RaycastHit _hitInfo;
+	bool _isHit;
+	[SerializeField] AudioClip _shotSound;
+	float _timeAfterShot;　
+	bool _canShoot;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		_canShoot = true;
+		// _bullet.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		_ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		_isHit = Physics.Raycast (_ray, out _hitInfo, MAX_RAY_DISTANCE);
+		
+		TurnGun();
+		Shoot();
+	}	
+
+	void TurnGun()
+	{
+		if(_isHit)
+		{
+			transform.parent.LookAt(_hitInfo.point);
+			transform.parent.Rotate(4,0,0);
+			Debug.DrawLine(transform.position, _hitInfo.point, Color.red);
+		}
+		else
+		{
+			Vector3 vec = _ray.GetPoint(MAX_RAY_DISTANCE) - transform.position;
+			transform.parent.forward = vec.normalized;
+			Debug.DrawLine(transform.position, _ray.GetPoint(MAX_RAY_DISTANCE), Color.red);
+		}
+	}
+
+	void Shoot()
+	{
+		if (Input.GetMouseButtonDown (0) && _canShoot)
+		{
+			// shot interval
+			StartCoroutine("WaitShotInterval");
+			
+			// 銃弾
+			_bulletPool.ShootBullet();
+			// 火花パーティクル
+			_gunFireParticle.Play();
+			// 銃声
+			AudioSource.PlayClipAtPoint(_shotSound, transform.position);
+			
+			if (_isHit) 
+			{
+				if (_hitInfo.transform.tag == _hitTarget.name)
+				{
+					//	ヒット時の処理;
+					Debug.Log("hit " + _hitTarget.name);
+				}
+				else
+				{
+					// 跳弾による火花エフェクト
+					GameObject _ricochetFire = Instantiate(_ricochetFirePrefab, _hitInfo.point, Quaternion.FromToRotation(Vector3.up, _hitInfo.normal));
+					_ricochetFire.GetComponent<ParticleSystem>().Play();
+					AudioSource.PlayClipAtPoint(_shotSound, _ray.GetPoint(MAX_RAY_DISTANCE));
+					Destroy(_ricochetFire, 0.1f);
+				}
+			}
+    	}
+	}
+
+	IEnumerator WaitShotInterval()
+	{
+		_canShoot = false;
+		yield return new WaitForSeconds(SHOT_INTERVAL);
+		_canShoot = true;
+	}
+}
