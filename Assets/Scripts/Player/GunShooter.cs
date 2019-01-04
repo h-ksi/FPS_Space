166b2de8c750e075999c79d8439e972d7ff56b3b
Kosei Hirano <mathko314@gmail.com>
2019-01-05 01:28:00 +0900

    eliminate unnecessary comments and add small changes on GunShooter.cs

--- Assets/Scripts/Player/GunShooter.cs
+++ Assets/Scripts/Player/GunShooter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GunShooter : MonoBehaviour 
+public class GunShooter : MonoBehaviour
 {
 	const float SHOT_INTERVAL = 0.1f;
 	const float MAX_RAY_DISTANCE = 100f;
@@ -10,85 +10,84 @@ public class GunShooter : MonoBehaviour
 	[SerializeField] BulletPool _bulletPool;
 	[SerializeField] GameObject _hitTarget;
 	[SerializeField] GameObject _ricochetFirePrefab;
-    [SerializeField] ParticleSystem _gunFireParticle;
-	
+	[SerializeField] ParticleSystem _gunFireParticle;
+
 	Ray _ray;
 	RaycastHit _hitInfo;
 	bool _isHit;
 	[SerializeField] AudioClip _shotSound;
 	float _timeAfterShot;　
 	bool _canShoot;
-	
+
 	// Use this for initialization
-	void Start () 
+	void Start ()
 	{
 		_canShoot = true;
-		// _bullet.gameObject.SetActive(false);
 	}
-	
+
 	// Update is called once per frame
-	void Update () 
+	void Update ()
 	{
 		_ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 		_isHit = Physics.Raycast (_ray, out _hitInfo, MAX_RAY_DISTANCE);
-		
-		TurnGun();
-		Shoot();
-	}	
 
-	void TurnGun()
+		TurnGun ();
+		Shoot ();
+	}
+
+	void TurnGun ()
 	{
-		if(_isHit)
+		if (_isHit)
 		{
-			transform.parent.LookAt(_hitInfo.point);
-			transform.parent.Rotate(4,0,0);
-			Debug.DrawLine(transform.position, _hitInfo.point, Color.red);
+			transform.parent.LookAt (_hitInfo.point);
+			transform.parent.Rotate (4, 0, 0);
+			Debug.DrawLine (transform.position, _hitInfo.point, Color.red);
 		}
 		else
 		{
-			Vector3 vec = _ray.GetPoint(MAX_RAY_DISTANCE) - transform.position;
+			Vector3 vec = _ray.GetPoint (MAX_RAY_DISTANCE) - transform.position;
 			transform.parent.forward = vec.normalized;
-			Debug.DrawLine(transform.position, _ray.GetPoint(MAX_RAY_DISTANCE), Color.red);
+			Debug.DrawLine (transform.position, _ray.GetPoint (MAX_RAY_DISTANCE), Color.red);
 		}
 	}
 
-	void Shoot()
+	void Shoot ()
 	{
 		if (Input.GetMouseButtonDown (0) && _canShoot)
 		{
 			// shot interval
-			StartCoroutine("WaitShotInterval");
-			
+			StartCoroutine ("WaitShotInterval");
+
 			// 銃弾
-			_bulletPool.ShootBullet();
+			_bulletPool.ShootBullet ();
 			// 火花パーティクル
-			_gunFireParticle.Play();
+			_gunFireParticle.Play ();
 			// 銃声
-			AudioSource.PlayClipAtPoint(_shotSound, transform.position);
-			
-			if (_isHit) 
+			AudioSource.PlayClipAtPoint (_shotSound, transform.position);
+
+			if (_isHit)
 			{
 				if (_hitInfo.transform.tag == _hitTarget.name)
 				{
 					//	ヒット時の処理;
-					Debug.Log("hit " + _hitTarget.name);
+					Debug.Log ("hit " + _hitTarget.name);
 				}
 				else
 				{
 					// 跳弾による火花エフェクト
-					GameObject _ricochetFire = Instantiate(_ricochetFirePrefab, _hitInfo.point, Quaternion.FromToRotation(Vector3.up, _hitInfo.normal));
-					_ricochetFire.GetComponent<ParticleSystem>().Play();
-					AudioSource.PlayClipAtPoint(_shotSound, _ray.GetPoint(MAX_RAY_DISTANCE));
-					Destroy(_ricochetFire, 0.1f);
+					GameObject _ricochetFire = Instantiate (_ricochetFirePrefab, _hitInfo.point, Quaternion.identity);
+					_ricochetFire.GetComponent<ParticleSystem> ().Play ();
+					AudioSource.PlayClipAtPoint (_shotSound, _ray.GetPoint (_hitInfo.distance));
+					Destroy (_ricochetFire, 0.1f);
 				}
 			}
-    	}
+		}
 	}
 
-	IEnumerator WaitShotInterval()
+	IEnumerator WaitShotInterval ()
 	{
 		_canShoot = false;
-		yield return new WaitForSeconds(SHOT_INTERVAL);
+		yield return new WaitForSeconds (SHOT_INTERVAL);
 		_canShoot = true;
 	}
 }
