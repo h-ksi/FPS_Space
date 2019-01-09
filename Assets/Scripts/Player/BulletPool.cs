cc1b4ec5700b3c71cb0d9a2ef1793e0dd164dede
Kosei Hirano <mathko314@gmail.com>
2019-01-09 13:31:42 +0900

    use a List<rigidbody> to cash rigidbodies of bullets in BulletPool.cs

--- Assets/Scripts/Player/BulletPool.cs
+++ Assets/Scripts/Player/BulletPool.cs
@@ -6,48 +6,51 @@ public class BulletPool : MonoBehaviour
 {
     const float SHOT_SPEED = 100f;
 
-    [SerializeField] GameObject _pollObject;    
+    [SerializeField] GameObject _pollObject;
+    List<GameObject> _pollList;
+    List<Rigidbody> _rigidbodyList;
     int _polledAmount = 20;
-    [SerializeField] List<GameObject> _pollList;
     int _readyNum = 0;
 
     // Use this for initialization
-    void Start()
+    void Start ()
     {
-        _pollList = new List<GameObject>();
+        _pollList = new List<GameObject> ();
+        _rigidbodyList = new List<Rigidbody> ();
 
         for (int i = 0; i < _polledAmount; i++)
         {
-            GameObject　_bullet = Instantiate(_pollObject, transform.position, Quaternion.identity);            
-            _bullet.SetActive(false);
+            GameObject　 _bullet = Instantiate (_pollObject, transform.position, Quaternion.identity);
+            _bullet.SetActive (false);
             _bullet.transform.parent = this.gameObject.transform;
-            _bullet.transform.Rotate(transform.right, 90);
-            _pollList.Add(_bullet);
+            _bullet.transform.Rotate (transform.right, 90);
+            _pollList.Add (_bullet);
+            _rigidbodyList.Add (_bullet.GetComponent<Rigidbody> ());
         }
     }
 
-    public void ShootBullet()
-    {                
-        if(_readyNum == _polledAmount)
+    public void ShootBullet ()
+    {
+        if (_readyNum == _polledAmount)
         {
             _readyNum = 0;
         }
 
         // その弾丸の初回使用時にアクティブにする
-        if(!_pollList[_readyNum].activeSelf)
+        if (!_pollList[_readyNum].activeSelf)
         {
-            _pollList[_readyNum].SetActive(true);
+            _pollList[_readyNum].SetActive (true);
         }
         // 再利用弾を銃口にセット
         else
         {
             _pollList[_readyNum].transform.position = transform.position;
             _pollList[_readyNum].transform.rotation = transform.rotation;
-            _pollList[_readyNum].transform.Rotate(transform.right, 90);
+            _pollList[_readyNum].transform.Rotate (transform.right, 90);
         }
-        
+
         // 発射
-        _pollList[_readyNum].GetComponent<Rigidbody>().velocity = transform.forward * SHOT_SPEED;
-        _readyNum++;         
+        _rigidbodyList[_readyNum].velocity = transform.forward * SHOT_SPEED;
+        _readyNum++;
     }
 }
